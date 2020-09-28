fd54d2318ea542d63865c2d136286957de02396e
Petu <60960104+Petu97@users.noreply.github.com>
2020-09-28 22:03:17 +0300

    reworked the server
    server uses now app.all to finish functions

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-var List = {};
-//These 2 are included in express library
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -10,54 +8,41 @@ app.listen(3000, () => {
   console.log("Server started on port 3000");
 });
 
-// GET request handling to /messages
-app.get("/messages/", (req, res) => {
-  console.log(req.body);
-  if (List[req.query.mi])
-    res.render("index.ejs", {
-      messagecontent: `Fetched message: "${
-        List[req.query.mi].message
-      }" where id: "${req.query.mi}"`,
-      methodtype: req.method,
-    });
-  else res.end();
+app.all(["/messages/", "/messages/:id"], (req, res) => {
+  //Get request
+  if (req.method === "GET")
+    returnpage(
+      res,
+      `Here's the list: "The list should be here..."`,
+      req.params.id
+    );
+  //Post request
+  else if (req.method === "POST")
+    returnpage(
+      res,
+      `Added title: "${req.body.messagetitle}" and content: "${req.body.messagecontent}"`,
+      req.params.id
+    );
+  //put request
+  else if (req.method === "PUT")
+    returnpage(
+      res,
+      `Updated title: "${req.body.messagetitle}" and content: "${req.body.messagecontent}"`,
+      req.params.id
+    );
+  //delete request
+  else if (req.method === "DELETE")
+    returnpage(res, `Deleting all messages hahaa!`, req.params.id);
 });
 
-// POST request handling to /messages
-app.post("/messages/", (req, res) => {
-  if (req.query.mi && req.query.mc) {
-    List[req.query.mi] = {
-      message: req.query.mc.toString(),
-    };
+//render page to user, res = client, str = string to be sent, id = given id
+function returnpage(res, str, id) {
+  if (id == null)
     res.render("index.ejs", {
-      messagecontent: `Added message: "${req.query.mc}" where id: "${req.query.mi}"`,
-      methodtype: req.method,
+      messagecontent: str,
     });
-  } else res.end();
-});
-
-// PUT request handling to /messages
-app.put("/messages/", (req, res) => {
-  if (req.query.mi && req.query.mc) {
-    List[req.query.mi] = {
-      message: req.query.mc.toString(),
-    };
+  else
     res.render("index.ejs", {
-      messagecontent: `Updated message to: "${req.query.mc}" where id: "${req.query.mi}"`,
-      methodtype: req.method,
+      messagecontent: "ID: " + id + " " + str,
     });
-  } else res.end();
-});
-
-// DELETE request handling to /messages
-app.delete("/messages/", (req, res) => {
-  if (List[req.query.mi]) {
-    res.render("index.ejs", {
-      messagecontent: `Deleting message: "${
-        List[req.query.mi].message
-      }" where id: "${req.query.mi}"`,
-      methodtype: req.method,
-    });
-    delete List[req.query.mi];
-  } else res.end();
-});
+}
